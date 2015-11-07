80cac31f2a01a3561c0b294fb41e0f1edbf64a2f
Preetesh Jain <preeteshjain17@gmail.com>
2015-11-08 02:13:35 +0530

    Changes to the front page

--- application/views/templates/_footer.php
+++ application/views/templates/_footer.php
@@ -1,3 +1,11 @@
-    <em>Flying Dogs &copy; 2015</em>
+    <div class="footer">
+        <div class="container">
+            <div>
+                <p>Notix is a project by Preetesh Jain, Varun Shrivastava, Shubham Atkare and Dino Jewel.</p>
+                <br><hr><br>
+                <p>Flying Dogs &copy; 2015</p>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
