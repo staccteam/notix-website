3bfe38edcf24d9d6348a4becd0db1839b0d4c180
vslala <varunshrivastava007@gmail.com>
2015-11-08 19:55:28 +0530

    stash reset

---
+++ application/views/site/home.php
@@ -0,0 +1,17 @@
+<div class="home">
+    <section class="parallax">
+        <p>An all new way to connect is here.</p>
+    </section>
+    <div class="container">
+        <div class="login-form">
+            <p>Faculty Login</p>
+            <form>
+                <div>
+                    <input type="text" name="userid" class="form-fields" placeholder="username/email"></input><br>
+                    <input type="password" name="password" class="form-fields" placeholder="password"></input><br>
+                </div>
+                <input type="submit" class="login-btn" name="login-btn" value="Login"></input>
+            </form>
+        </div>
+    </div>
+</div>
