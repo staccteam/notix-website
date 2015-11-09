bedd7db30a2e7c9678d145d8df9a44c42d7ebba7
Preetesh Jain <preeteshjain17@gmail.com>
2015-11-10 03:23:06 +0530

    Added a light grid system

--- application/views/site/home.php
+++ application/views/site/home.php
@@ -2,8 +2,8 @@
     <section class="parallax">
         <p>An all new way to connect is here.</p>
     </section>
-    <div class="container">
-        <div class="login-form">
+    <div class="container group">
+        <div class="login-form col span_1_of_2">
             <p>Faculty Login</p>
             <?= form_open('auth/faculty/login'); ?>
                 <div>
@@ -13,5 +13,14 @@
                 <input type="submit" class="login-btn" name="login-btn" value="Login"></input>
             <?= form_close(); ?>
         </div>
+        <div class="message-box col span_1_of_2">
+            <div class="wrapper">
+                <h1>What is Notix?</h1>
+                <p>
+                    Notix is a platform for instantly sharing all the news and happenings in the college. Currently, this website serves only as an admin console to the faculty members, but we are inclined to extend it to serve multiple purposes, such as, forums and personas. Students of HCET, please download the official Notix Android App from the Play Store: <a target="_blank" style="color:black;" href="#"><i class="fa fa-download"></i></a>
+                </p>
+            </div>
+        </div>
     </div>
+</div>
 </div>
