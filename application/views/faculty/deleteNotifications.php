a283bc3458a40d03e397ae0b67c89c773df47e75
Preetesh Jain <preeteshjain17@gmail.com>
2015-11-19 00:32:06 +0530

    Updates to Faculty console

--- application/views/faculty/deleteNotifications.php
+++ application/views/faculty/deleteNotifications.php
@@ -1,13 +1,30 @@
 <div class="faculty-deleteNotifications">
     <div class="container">
-    	<?= $this->session->userdata('first_name'); ?>
+        <?= $this->session->userdata('first_name'); ?>
         <h1>Notification List</h1><br>
+        <?php if (isset($notifications[0])): ?>
+            <table>
+                <tr>
+                    <th>Title</th>
+                    <th>Message</th>
+                    <th class="hidable">Created at</th>
+                    <th>Delete</th>
+                </tr>
+                <?php foreach ($notifications as $n): ?>
+                <tr>
+                    <td><?= $n['title']; ?></td>
+                    <td><?= $n['message']; ?></td>
+                    <?php
+                        $mysql_timestamp = strtotime($n['created_at']);
+                        $timestamp = date('M d, Y | h:i A', $mysql_timestamp);
+                    ?>
+                    <td class="hidable"><?php echo $timestamp; ?></td>
+                    <td><a href="#"><i class="fa fa-trash-o"></i></a></td>
+                </tr>
+                <?php endforeach; ?>
+            </table>
+        <?php else: ?>
+            <p>No entries found.</p>
+        <?php endif; ?>
     </div>
-    <?php if (isset($notifications[0])): ?>
-    	<ul>
-    		<?php foreach ($notifications as $n): ?>
-    		<li><?= $n['title']; ?></li>
-    		<?php endforeach; ?>
-    	</ul>
-	<?php endif; ?>
 </div>
--- application/views/faculty/profile.php
+++ application/views/faculty/profile.php
@@ -1,12 +1,26 @@
 <div class="faculty-profile">
     <div class="container group">
-        <?= form_open_multipart('faculty/uploadProfilePicture'); ?>
-      		<p>
-                <label for="msg-attachment">Upload Profile Picture: </label>
-                <input name="profile-picture" id="profile-picture" type="file" accept=".jpg,.jpeg,.png"/>
-                <p style="color:gray;">Allowed File types: .jpg, .jpeg and .png</p>
-            </p>
-            <p><input type="submit" value="Upload"/></p>
-        <?= form_close(); ?>
+        <section class="col span_1_of_4 profile-pic-form">
+            <?php if(isset($faculty[0]['image_url'])): ?>
+            <img style="border-radius:50%;border:2px solid #ccc;background:#eee;" src="<?php echo $faculty[0]['image_url']; ?>">
+            <?php else: ?>
+            <img style="border-radius:50%;border:2px solid #ccc;background:#eee;" src="<?= base_url(); ?>/img/def_avatar.svg">
+            <?php endif; ?>
+            <?= form_open_multipart('faculty/uploadProfilePicture'); ?>
+                <p>
+                    <label for="msg-attachment">Change your Profile Picture</label>
+                    <input name="profile-picture" id="profile-picture" type="file" accept=".jpg,.jpeg,.png"/>
+                    <p style="color:gray;">Allowed File types: .jpg, .jpeg and .png</p>
+                </p>
+                <p><input type="submit" value="Upload"/></p>
+            <?= form_close(); ?>
+        </section>
+        <section class="col span_1_of_4 profile-form">
+            <ul>
+                <?php foreach ($faculty[0] as $fd): ?>
+                    <li>First Name: <?php echo $fd['first_name']; ?></li><br>
+                <?php endforeach; ?>
+            </ul>
+        </section>
     </div>
 </div>
