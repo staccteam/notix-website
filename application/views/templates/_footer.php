871415642e88c404d78d3628c3f0a25d30156b14
Preetesh Jain <preeteshjain@users.noreply.github.com>
2016-04-26 20:43:35 +0530

    Update _footer.php

--- application/views/templates/_footer.php
+++ application/views/templates/_footer.php
@@ -15,7 +15,7 @@
                 <p>Notix is a project for</p><br>
                 <p><a target="_blank" href="http://hcet.hitkarini.com"><img src="<?= base_url(); ?>img/hcet-logo.png"></a></p>
                 <br><hr><br>
-                <p>Made with <i class="fa fa-heart"></i> in Jabalpur | An open source project by <a target="_blank" href="//facebook.com/FlyingDogsIndia">Flying Dogs</a></p>
+                <p>Made with <i class="fa fa-heart"></i> in Jabalpur | An open source project by <a target="_blank" href="//facebook.com/staccteam">Stacc</a></p>
             </div>
         </div>
     </div>
