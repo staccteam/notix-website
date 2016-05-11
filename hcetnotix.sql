8fee5b9202500cfd8051181bdb14ab55b8c8f962
Varun <varunshrivastava007@gmail.com>
2016-05-11 17:45:32 +0530

    add database

--- hcetnotix.sql
+++ hcetnotix.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 11, 2016 at 12:51 PM
+-- Generation Time: May 11, 2016 at 02:14 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.20
 
@@ -43,6 +43,19 @@ INSERT INTO `notix_admins` (`username`, `password`, `created_at`, `updated_at`) 
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `notix_attachments`
+--
+
+CREATE TABLE `notix_attachments` (
+  `id` int(11) NOT NULL,
+  `notification_id` int(11) NOT NULL,
+  `file_name` varchar(500) NOT NULL,
+  `file_url` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `notix_branches`
 --
 
@@ -50,6 +63,15 @@ CREATE TABLE `notix_branches` (
   `branch` varchar(255) NOT NULL,
   `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `notix_branches`
+--
+
+INSERT INTO `notix_branches` (`branch`, `id`) VALUES
+('CS', 1),
+('MECH', 2),
+('ALL', 3);
 
 -- --------------------------------------------------------
 
@@ -70,6 +92,37 @@ CREATE TABLE `notix_faculties` (
   `updated_at` datetime NOT NULL,
   `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `notix_faculties`
+--
+
+INSERT INTO `notix_faculties` (`first_name`, `last_name`, `email`, `mobile`, `username`, `password`, `branch`, `status`, `created_at`, `updated_at`, `id`) VALUES
+('Pooja', 'Chamar', 'poojachamar@gmail.com', '9876987654', 'pooja', '$2y$10$px5AGjMIE3dfoppW5e7nOuODubbz/FhlMJeqQoVN.4J.FB.5ubZ9O', '1', 1, '0000-00-00 00:00:00', '2016-05-11 13:12:59', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notix_notifications`
+--
+
+CREATE TABLE `notix_notifications` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `title` varchar(1000) NOT NULL,
+  `message` text NOT NULL,
+  `attachment` varchar(500) NOT NULL,
+  `branch_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `notix_notifications`
+--
+
+INSERT INTO `notix_notifications` (`id`, `username`, `title`, `message`, `attachment`, `branch_id`, `created_at`, `updated_at`) VALUES
+(1, 'pooja', 'First Notification', 'Bas yahi h ab toh', '', 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -108,6 +161,12 @@ ALTER TABLE `notix_faculties`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `notix_notifications`
+--
+ALTER TABLE `notix_notifications`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -115,12 +174,17 @@ ALTER TABLE `notix_faculties`
 -- AUTO_INCREMENT for table `notix_branches`
 --
 ALTER TABLE `notix_branches`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `notix_faculties`
 --
 ALTER TABLE `notix_faculties`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `notix_notifications`
+--
+ALTER TABLE `notix_notifications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
